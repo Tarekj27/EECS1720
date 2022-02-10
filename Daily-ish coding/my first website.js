b0b0a9703c04fe9613688725ce5368a859de4719
Tarekj27 <T2002jk@gmail.com>
2022-02-10 04:11:20 -0500

    title color picker

---
+++ Daily-ish coding/my first website.js
@@ -0,0 +1,6 @@
+function showHex(){
+    var hex =  document.querySelector("#color_hex_value"),
+        h1 = document.querySelector("h1");
+  
+    h1.style.color = document.querySelector("#color_picker").value;
+  }
